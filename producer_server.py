e6016874d3c775f8aff282c1883d093ea69aee97
andrewpaiven <GtitcIh2k4B>
2020-01-26 15:11:28 +0100

    Completed part 1: streaming file to kafka topic

--- producer_server.py
+++ producer_server.py
@@ -13,13 +13,13 @@ class ProducerServer(KafkaProducer):
     #TODO we're generating a dummy data
     def generate_data(self):
         with open(self.input_file) as f:
-            for line in f:
-                message = self.dict_to_binary(line)
+            data = json.load(f)
+            for item in data:
+                message = self.dict_to_binary(item)
                 # TODO send the correct data
-                self.send()
+                self.send(self.topic, message)
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode("utf-8")
